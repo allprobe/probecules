810648ed18fad599e47ee73c44837e40f78c626b
roi <roi@Lenovo-G5080>
2015-12-14 02:36:46 +0200

    logging update (reverted from commit 854b610ec2bcf55fb9e0e733258642b2fa3e1c01)

--- lycus/src/lycus/BashColor.java
+++ lycus/src/lycus/BashColor.java
@@ -1,11 +1,11 @@
 package lycus;
 
 public interface BashColor {
-	String RED=" '\\e033[0;31m' ";
-    String BLUE = " '\\e033[0;34m' ";
-    String GREEN = " '\\e033[0;32m' ";
-    String WHITE = " '\\e033[0;37m' ";
-    String YELLOW = " '\\e033[0;33m' ";
-    String PURPLE = " '\\e033[0;35m' ";
-    String NO_COLOR=" '\\e033[0m' ";
+	String RED="'\033[0;31m'";
+    String BLUE = "'\033[0;34m'";
+    String GREEN = "'\033[0;32m'";
+    String WHITE = "'\033[0;37m'";
+    String YELLOW = "'\033[0;33m'";
+    String PURPLE = "'\033[0;35m'";
+    String NO_COLOR="'\033[0m'";
 }
