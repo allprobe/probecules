bbd27441da01e470ffecdb381b1b21e44587b5f1
Oren <oren@allprobe.com>
2016-01-19 19:51:32 +0200

    Updates new

---
+++ lycus/src/lycus/Constants.java
@@ -0,0 +1,9 @@
+package lycus;
+
+public class Constants {
+	public final static String  https = "https://";
+	public final static String  http = "http://";
+	public final static String  get = "GET";
+	public final static String  put = "PUT";
+
+}
