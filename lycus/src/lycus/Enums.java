8091343e334e64f3aec9965dd98defe3f4b269ee
Oren <oren@allprobe.com>
2016-02-09 15:03:32 +0200

    .

--- lycus/src/lycus/Enums.java
+++ lycus/src/lycus/Enums.java
@@ -3,14 +3,51 @@ package lycus;
 public class Enums {
 	public static enum ApiAction
 	{
-		InitServer,GetServerMemoryDump,FlushServerMemory,InsertDatapointsBatches,PutEvents,GetServerLiveEvents,GetThreadsUpdates
+		InitServer,
+		GetServerMemoryDump,
+		FlushServerMemory,
+		InsertDatapointsBatches,
+		PutEvents,
+		GetServerLiveEvents,
+		GetThreadsUpdates, 
+		DevGetThreadsUpdates
 	}
 		public static enum DiscoveryElementType
 		{
 		nics,disks	
-		}
+	}
 	public static enum SnmpStoreAs
 	{
 		asIs,delta
 	}
+	
+	
+	public static enum UpdateType
+	{
+		pod,   // Update Probe
+		npob,  // New Probe
+		pdel,  // Delete probe
+		utrig, // Update trigger
+		ntrig, // New trigger
+		dtrig, // Delete trigger
+		updh,  // Update host
+		delh,  // Delete host
+		updt,  // Update template
+		delt,  // Delete template
+		updbucket, // Update bucket
+		delbucket, // Delete bucket
+		updsnmp,   // Update snmp
+		delsnmp,   // Delete snmp
+		udisc,     // Update discovery
+		ude,       // Update element
+		rdisc      // Delete discovery
+	}
+	
+	public static enum Action {
+		New,
+		Update,
+		Delete
+	}
+
+	public static final String Action = null;
 }
