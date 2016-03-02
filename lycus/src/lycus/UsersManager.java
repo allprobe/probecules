8b58453b2ea2a8ed1b2d7172c9386412fc8cbdc1
roi <roi@Lenovo-G5080>
2016-03-02 13:45:11 +0200

    . (reverted from commit b6bc1d85f37f7e7caeccc9458931d8f94cdf4994)

--- lycus/src/lycus/UsersManager.java
+++ lycus/src/lycus/UsersManager.java
@@ -262,12 +262,14 @@ public class UsersManager {
 					break;
 				}
 				case Constants.discovery: {
+					probeParams.discovery_elements_interval = Integer
 							.parseInt(probeKeyJson.get("element_interval").toString());
 					probeParams.discovery_trigger_code = Integer
-					probeParams.discovery_trigger_x = (String) probeKeyJson.get("discovery_trigger_x_value");
-					probeParams.snmp_unit= (String) probeKeyJson.get("discovery_trigger_unit");
-					probeParams.discovery_trigger_id = (String) probeKeyJson.get("discovery_trigger_id");
-					probeParams.discovery_trigger_severity = (String) probeKeyJson.get("discovery_trigger_severity");
+							.parseInt(probeKeyJson.get("discovery_trigger").toString());
+					probeParams.discovery_trigger_x = probeKeyJson.get("discovery_trigger_x_value").toString();
+					probeParams.snmp_unit = (String) probeKeyJson.get("discovery_trigger_unit");
+					probeParams.discovery_trigger_id = (String) probeKeyJson.get("trigger_id");
+					probeParams.discovery_trigger_severity = (String) probeKeyJson.get("trigger_severity");
 					break;
 				}
 				case Constants.rbl: {
