02e2f840ff93c86fc88a667df7c163eb2d78ed82
roi <roi@Lenovo-G5080>
2016-03-03 19:50:51 +0200

    snmpnicprobe

---
+++ lycus/src/lycus/Probes/SnmpNicProbe.java
@@ -0,0 +1,30 @@
+package lycus.Probes;
+
+import java.util.UUID;
+
+import org.snmp4j.smi.OID;
+
+import GlobalConstants.Enums.SnmpStoreAs;
+import GlobalConstants.SnmpDataType;
+import lycus.SnmpUnit;
+import lycus.User;
+
+public class SnmpNicProbe extends SnmpProbe {
+
+	private long ifSpeed;
+	
+	public SnmpNicProbe(User user, String probe_id, UUID template_id, String name, long interval, float multiplier,
+			boolean status, OID oid, SnmpDataType dataType, SnmpUnit unit,long ifSpeed) {
+		super(user, probe_id, template_id, name, interval, multiplier, status, oid, dataType, unit, SnmpStoreAs.deltaBytesPerSecond);
+		this.setIfSpeed(ifSpeed);
+	}
+
+	public long getIfSpeed() {
+		return ifSpeed;
+	}
+
+	public void setIfSpeed(long ifSpeed) {
+		this.ifSpeed = ifSpeed;
+	}
+
+}
