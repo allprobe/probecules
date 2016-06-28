b07a6e1f97d0d89789f94d8309366b47e9058fd9
Oren <oren@allprobe.com>
2016-06-28 21:28:51 +0300

    version 219

--- lycus/src/lycus/LycusMain.java
+++ lycus/src/lycus/LycusMain.java
@@ -42,8 +42,8 @@ import com.gargoylesoftware.htmlunit.WebClient;;
 public class LycusMain {
 
 	public static void main(String[] args) {
-		System.out.println("Starting Probecules Version: 0.218");
-		Logit.LogCheck("Starting Probecules Version: 0.218");
+		System.out.println("Starting Probecules Version: 0.219");
+		Logit.LogCheck("Starting Probecules Version: 0.219");
 		
 		System.setProperty("log4j.debug","true");
 		if (args.length == 0 || args[0] == "")
