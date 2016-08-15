d22756f31c978c5286a9d5b0b123f5ccc67d4b3b
roi <roi@Lenovo-G5080>
2016-08-15 16:22:14 +0300

    snmp unit new format

--- lycus/src/GlobalConstants/SnmpUnit.java
+++ lycus/src/GlobalConstants/SnmpUnit.java
@@ -3,7 +3,7 @@ package GlobalConstants;
 public enum SnmpUnit {
 	// b(1L), B(8L), kb(1024L), KB(8192L), mb(1048576L), MB(8388608L),
 	// gb(1073741824L), GB(8589934592L), as_is(1);
-	b, B, kb, KB, mb, MB, gb, GB, as_is;
+	b, B, Kb, KB, Mb, MB, Gb, GB, as_is;
 
 	public static long getBasic(long value, SnmpUnit unit) {
 		switch (unit) {
@@ -13,15 +13,15 @@ public enum SnmpUnit {
 			return value;
 		case B:
 			return value * 8;
-		case kb:
+		case Kb:
 			return value * 1024;
 		case KB:
 			return value * 8192;
-		case mb:
+		case Mb:
 			return value * 1048576;
 		case MB:
 			return value * 8388608;
-		case gb:
+		case Gb:
 			return value * 1073741824;
 		case GB:
 			return value * 8589934592L;
