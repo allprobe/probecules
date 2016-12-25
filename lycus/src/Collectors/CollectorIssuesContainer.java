b40299651c5a4765c97f59df7bcf190185b14550
Oren <oren@allprobe.com>
2016-12-25 18:49:02 +0200

    Adding logs for addIssue

--- lycus/src/Collectors/CollectorIssuesContainer.java
+++ lycus/src/Collectors/CollectorIssuesContainer.java
@@ -40,48 +40,55 @@ public class CollectorIssuesContainer implements ICollectorIssuesContainer {
 
 	@Override
 	public synchronized void addIssue(Host host, Enums.CollectorType type, String issue, int issueState) {
-		if (host.getHostIp().contains("62.90.132.124"))
-			Logit.LogDebug("BP");
-		String hostId = host.getHostId().toString();
-		if (issueState == 1) {
-			if (this.liveIssues.containsKey(hostId)) {
-				if (this.liveIssues.get(hostId).contains(type))
-					return;
-				else
-					this.liveIssues.get(hostId).add(type);
-			} else {
-				HashSet<Enums.CollectorType> newIssueTable = new HashSet<Enums.CollectorType>();
-				newIssueTable.add(type);
-				this.liveIssues.put(hostId, newIssueTable);
-			}
-		} else {
-			if (!this.liveIssues.containsKey(hostId)
-					|| !this.liveIssues.get(hostId).contains(type))
-				return;
-			else {
-				this.liveIssues.get(hostId).remove(type);
-				if (this.liveIssues.get(hostId).size() == 0)
-					this.liveIssues.remove(hostId);
-			}
-		}
-		JSONObject issueObject = new JSONObject();
+		String hostId = "";
 		String userId = "";
 		try {
-			userId = host.getUserId();
-			if (userId != null)
-				issueObject.put("user_id", userId);
+			if (host.getHostIp().contains("62.90.132.124"))
+				Logit.LogDebug("BP");
+			hostId = host.getHostId().toString();
+			if (issueState == 1) {
+				if (this.liveIssues.containsKey(hostId)) {
+					if (this.liveIssues.get(hostId).contains(type))
+						return;
+					else
+						this.liveIssues.get(hostId).add(type);
+				} else {
+					HashSet<Enums.CollectorType> newIssueTable = new HashSet<Enums.CollectorType>();
+					newIssueTable.add(type);
+					this.liveIssues.put(hostId, newIssueTable);
+				}
+			} else {
+				if (!this.liveIssues.containsKey(hostId)
+						|| !this.liveIssues.get(hostId).contains(type))
+					return;
+				else {
+					this.liveIssues.get(hostId).remove(type);
+					if (this.liveIssues.get(hostId).size() == 0)
+						this.liveIssues.remove(hostId);
+				}
+			}
+			JSONObject issueObject = new JSONObject();
+			
+			try {
+				userId = host.getUserId();
+				if (userId != null)
+					issueObject.put("user_id", userId);
+			} catch (Exception e) {
+				Logit.LogWarn("Error getting userId from host: " + hostId + " userId: " + userId);
+				e.printStackTrace();
+			}
+			issueObject.put("host_id", hostId);
+			issueObject.put("type", type.toString());
+			issueObject.put("issue_info", issue);
+			issueObject.put("issue_status", issueState);
+			issueObject.put("time", System.currentTimeMillis());
+
+			count++;
+			issuesArray.add(issueObject);
 		} catch (Exception e) {
-			Logit.LogWarn("Error getting userId from host: " + hostId + " userId: " + userId);
+			Logit.LogError("CollectorIssuesContainer - addIssue()", "Error adding issue. Host:" + hostId + ", UserId: " + userId, e);
 			e.printStackTrace();
 		}
-		issueObject.put("host_id", hostId);
-		issueObject.put("type", type.toString());
-		issueObject.put("issue_info", issue);
-		issueObject.put("issue_status", issueState);
-		issueObject.put("time", System.currentTimeMillis());
-
-		count++;
-		issuesArray.add(issueObject);
 	}
 
 	public void clearAll() {
