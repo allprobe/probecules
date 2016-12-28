aeeaad2c91ca30b006f9602290a87f7a5b97f6a3
roi <roi@Lenovo-G5080>
2016-12-28 10:29:22 +0200

    deep web output exceptions fix

--- lycus/phantomjs/netsniff.js
+++ lycus/phantomjs/netsniff.js
@@ -142,4 +142,16 @@ if (system.args.length === 1) {
             phantom.exit();
         }
     });
+
+   page.onError = function(msg, trace) {
+	var msgStack = ['ERROR: ' + msg];
+	if (trace && trace.length) {
+		msgStack.push('TRACE:');
+        	trace.forEach(function(t) {
+            		msgStack.push(' -> ' + t.file + ': ' + t.line + (t.function ? ' (in function "' + t.function + '")' : ''));
+        	});
+    	}
+	// uncomment to log into the console 
+	// console.error(msgStack.join('\n'));
+   };
 }
