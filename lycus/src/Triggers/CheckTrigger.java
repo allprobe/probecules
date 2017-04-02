4858c638abaa82a755a8984bdfdffa99b86ef20b
Roi <roi@allprobe.net>
2017-04-02 14:25:57 +0300

    check trigger for snmp string (reverted from commit 7dd043613e9933ababba0cc74092d72861fc9563)

--- lycus/src/Triggers/CheckTrigger.java
+++ lycus/src/Triggers/CheckTrigger.java
@@ -43,17 +43,15 @@ public class CheckTrigger {
 					+ triggerCondition.getCondition().toString());
 
 			try {
-				
-				if (result instanceof SnmpResult && ((SnmpResult) result).getNumData() == null
-						&& ((SnmpResult) result).getData() != null) {
-					String xValue = ((SnmpResult) result).getData();
-					if (!isNoFunctionConditionMet(triggerCondition, xValue))
-						return false;
-					else
-						return true;
-				}
-				
-
+				// if (result instanceof SnmpResult && ((SnmpResult)
+				// result).getNumData() == null
+				// && ((SnmpResult) result).getData() != null) {
+				// String xValue = ((SnmpResult) result).getData();
+				// if (!isNoFunctionConditionMet(triggerCondition, xValue))
+				// return false;
+				// } else {
+				// if (result instanceof SnmpResult)
+				// Logit.LogDebug("xxx");
 				Double xValue = getDouble(triggerCondition.getxValue());
 
 				XvalueUnit resultUnit = result.getResultUnit(triggerCondition.getElementType().toString());
