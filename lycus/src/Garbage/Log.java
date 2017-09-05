247e1d68288495127764d0730ea8360f439ad819
Roi <roi@allprobe.net>
2017-09-05 17:29:01 +0300

    dfsdfs

--- lycus/src/Garbage/Log.java
+++ lycus/src/Garbage/Log.java
@@ -162,7 +162,7 @@ public class Log {
         this.exception = exception;
     }
 
-    // </editor-fold>gedfwsheghkjkjlhdfdfgvdfgsdfsdf
+    // </editor-fold>gedfwsheghdfdfgvdfgsdfsdf
 
     
     
